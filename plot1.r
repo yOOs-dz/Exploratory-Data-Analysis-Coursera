f29a609fcdd03ec173b76e8a5c5c023889b13c9b
yOOs-dz <74448776+yOOs-dz@users.noreply.github.com>
2020-11-25 14:10:37 +0100

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -3,14 +3,14 @@
 getwd()
 setwd("C:/Users/Youcef/Downloads/EDA_coursera/exdata_data_household_power_consumption")
 
-#Reading "household_power_consumption.txt" and defining a command "table1" to call it.
+#Read the dataset "household_power_consumption.txt" file and define a command "table1" to call it.
 table1 <- read.table("household_power_consumption.txt", header=TRUE, sep=";", dec=".",na.string="?")#reading table, containing a header. Definition of the *.txt separator, the decimal symbol "." and the non-value strings.
 head(table1)
 
-#Converting "Date" character values to date format. Defining a new variable "Date1" to put new values into (without loosing original ones).
+#Convert the variable "Date" character values to date format. For that, define a new variable "Date1" to put new values into (without loosing original ones).
 Date1 <- strptime(table1$Date,format="%d/%m/%Y")
 
-#Make a "table2" with desired subsets : Electrical Consumption between 01-02-2007 and 02-02-2007.
+# Define the final dataset "table2" with desired subsets : Electrical Consumption between 01-02-2007 and 02-02-2007.
 table2 <- subset(table1,Date1>="2007-02-01" & Date1<="2007-02-02")
 
 #Check results by "head()" comment
@@ -19,7 +19,7 @@ head(table2)
 attach(table2)
 #Plot 01
 
-#Making a histogram
+#Build the histogram plot1
 hist(table2$Global_active_power,col="red",main="Global Active Power",xlab ="Global Active Power (Kilowatts)")
 
 #Converting resulted plot to *.png
@@ -27,5 +27,3 @@ dev.cur()
 dev.copy(png,"plot1.png",height=480,width=480)
 dev.off()
 detach(table2)
-
-
